94e04d9789074a0bacd7bd08adf4bb8b12a8fe37
divyachandrapandi <divya4shivalaya@gmail.com>
2025-05-23 20:45:23 +0530

    Sedn emails

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,23 +1,22 @@
 import { Router } from 'express';
-// import authorize from '../middlewares/auth.middleware.js'
-// import {
-//     createSubscription,
-//     getUserSubscriptions,
-// } from '../controllers/subscription.controller.js'
+import authorize from '../middlewares/auth.middleware.js'
+import {
+    createSubscription,
+    getUserSubscriptions,
+    getAllSubscriptions
+} from '../controllers/subscription.controller.js'
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get('/', (req, res) => res.send({ title: 'GET all subscriptions' }));
+subscriptionRouter.get('/', getAllSubscriptions);
+subscriptionRouter.post('/', authorize, createSubscription);
+subscriptionRouter.get('/:id', authorize, getUserSubscriptions);
 
 subscriptionRouter.get('/:id', (req, res) => res.send({ title: 'GET subscription details' }));
-
-// subscriptionRouter.post('/', authorize, createSubscription);
 
 subscriptionRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE subscription' }));
 
 subscriptionRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE subscription' }));
-
-// subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions);
 
 subscriptionRouter.put('/:id/cancel', (req, res) => res.send({ title: 'CANCEL subscription' }));
 
