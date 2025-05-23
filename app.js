d332351adae3638d320fa50f4d4485be1312243d
divyachandrapandi <divya4shivalaya@gmail.com>
2025-05-23 10:34:17 +0530

    Added Routes

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import express from 'express';
 
+import { PORT, DB_URI } from './config/env.js';
+
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabase from './database/mongodb.js'
+
 const app = express();
 
-import { PORT } from './config/env.js';
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
 
 app.get('/', (req, res) =>{
     console.log("hello");
@@ -10,11 +20,11 @@ app.get('/', (req, res) =>{
 })
 
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log("Subscription tracker API is running on port 3000");
+    await connectToDatabase();
 });
 
 
 export default app;
-
 
--- config/env.js
+++ config/env.js
@@ -5,8 +5,8 @@ config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 export const {
     PORT,
     NODE_ENV,
+    DB_URI,
     // SERVER_URL,
-    // DB_URI,
     // JWT_SECRET, JWT_EXPIRES_IN,
     // ARCJET_ENV, ARCJET_KEY,
     // QSTASH_TOKEN, QSTASH_URL,
