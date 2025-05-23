df311d301c6701687ed1ffd8735d9c9cc6462976
divyachandrapandi <divya4shivalaya@gmail.com>
2025-05-23 19:25:41 +0530

    Arcjet middleware for Bot detection

--- config/env.js
+++ config/env.js
@@ -9,7 +9,8 @@ export const {
     // SERVER_URL,
     JWT_SECRET,
     JWT_EXPIRES_IN,
-    // ARCJET_ENV, ARCJET_KEY,
+    ARCJET_ENV,
+    ARCJET_KEY,
     // QSTASH_TOKEN, QSTASH_URL,
     // EMAIL_PASSWORD,
 } = process.env;
